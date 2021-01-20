bab993ed9c2de59f0b6bd9d2a8ac0d3cd0402312
szuwgh <895340293@qq.com>
2021-01-20 17:56:26 +0800

    save

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ mod lib;
 use crate::lib::libbpf;
 
 fn main() {
-    libbpf::new_bpf("./trace.elf");
+    libbpf::new_bpf("/lib/modules/5.9.12-1.el7.elrepo.x86_64/build/main.elf");
     libbpf::bpf_attach_kprobe();
     println!("Hello, world!");
 }
