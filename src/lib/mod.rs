f9d89cb3c93316d6c597348efe981b8339107c9c
szuwgh <895340293@qq.com>
2021-02-10 17:46:33 +0800

    save

--- src/lib/mod.rs
+++ src/lib/mod.rs
@@ -1,1 +1,2 @@
 pub mod libbpf;
+pub use libbpf::*;
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,7 @@
 mod lib;
-use crate::lib::libbpf;
 
 fn main() {
-    libbpf::new_bpf("/lib/modules/5.9.12-1.el7.elrepo.x86_64/build/main.elf");
-    libbpf::bpf_attach_kprobe();
+    // lib::new_bpf("/lib/modules/5.9.12-1.el7.elrepo.x86_64/build/main.elf");
+    lib::bpf_attach_kprobe();
     println!("Hello, world!");
 }
-
-
-
