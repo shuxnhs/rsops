8fa440b757a881e097738e886460a31c3e6e9304
szuwgh <895340293@qq.com>
2021-04-29 15:12:59 +0800

    save

--- rsops/src/main.rs
+++ rsops/src/main.rs
@@ -3,8 +3,8 @@ mod module;
 use lib::libbpf;
 use module::bpf;
 fn main() {
-    libbpf::new_bpf("/lib/modules/5.11.6-1.el7.elrepo.x86_64/source/main.elf");
-    println!("------");
+    // libbpf::new_bpf("/lib/modules/5.11.6-1.el7.elrepo.x86_64/source/main.elf");
+    // println!("------");
     bpf::parse("/lib/modules/5.11.6-1.el7.elrepo.x86_64/source/main.elf");
     libbpf::bpf_attach_kprobe();
     println!("Hello, world!");
